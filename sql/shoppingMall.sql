5bced018d1e6acab34844ff72d21455d08c11e79
hwigyeompark <phg5590@gmail.com>
2018-05-28 09:58:31 +0900

    update sql

--- sql/shoppingMall.sql
+++ sql/shoppingMall.sql
@@ -1,3 +1,11 @@
-CREATE TABLE member{
-
-}
+CREATE TABLE member(
+    idx int AUTO_INCREMENT PRIMARY KEY,
+    id varchar(50),
+    password varchar(50),
+    name varchar(100),
+    homeNumber varchar(50),
+    phone varchar(50),
+    postCode varchar(50),
+    address varchar(100),
+    email varchar(50),
+);
