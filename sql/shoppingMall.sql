94900ff270e61b94300693a90c2b2a7796dc2f97
hwigyeompark <phg5590@gmail.com>
2018-05-28 10:17:23 +0900

    update sql

--- sql/shoppingMall.sql
+++ sql/shoppingMall.sql
@@ -2,10 +2,18 @@ CREATE TABLE member(
     idx int AUTO_INCREMENT PRIMARY KEY,
     id varchar(50),
     password varchar(50),
-    name varchar(100),
+    name varchar(50),
     homeNumber varchar(50),
     phone varchar(50),
     postCode varchar(50),
     address varchar(100),
     email varchar(50),
 );
+
+CREATE TABLE product(
+    pid int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50),
+    content varchar(50),
+    price int
+);
+
