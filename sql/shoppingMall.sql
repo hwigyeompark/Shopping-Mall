7640e3efbe2d5897c39c647e709739974aeb7cbd
hwigyeompark <phg5590@gmail.com>
2018-06-09 14:47:27 +0900

    update sql

--- sql/shoppingMall.sql
+++ sql/shoppingMall.sql
@@ -8,12 +8,33 @@ CREATE TABLE member(
     postCode varchar(50),
     address varchar(100),
     email varchar(50),
+    mileage int
 );
 
 CREATE TABLE product(
-    pid int AUTO_INCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     name varchar(50),
     content varchar(50),
     price int
 );
 
+CREATE TABLE cart(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    mid int,
+    pid int,
+    FOREIGN KEY(mid) REFERENCES member(id) ON UPDATE CASCADE,
+    FOREIGN KEY(pid) REFERENCES product(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE category(
+    name varchar(30),
+    pid int,
+    FOREIGN KEY(pid) REFERENCES member(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE oneProduct(
+    mid int,
+    pid int,
+    FOREIGN KEY(mid) REFERENCES member(id) ON UPDATE CASCADE,
+    FOREIGN KEY(pid) REFERENCES product(id) ON UPDATE CASCADE
+);
