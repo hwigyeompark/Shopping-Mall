38f52bb80b5bade3d2739c4ba23b7651f44af0cf
hwigyeompark <phg5590@gmail.com>
2018-06-11 14:58:59 +0900

    update main

--- src/component/MainNav.js
+++ src/component/MainNav.js
@@ -1,26 +1,31 @@
 import React, {Component} from 'react';
 import { Navbar } from 'reactstrap';
 import { Nav } from 'reactstrap';
-import { NavItem } from 'reactstrap';
+import { NavItem } from 'reactstrap'
+import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MainNav extends Component{
-    render(){
+    const code = (props) => {
         return(
-          <Navbar>
-              <Navbar.Header>
-                  <Navbar.Brand>
-                      <a href="#home">ShoppingMall</a>
-                  </Navbar.Brand>
-              </Navbar.Header>
-              <Nav>
-                  <NavItem href="#menu1">Menu1</NavItem>
-                  <NavItem href="#menu2">Menu2</NavItem>
-              </Nav>
-              <Nav pullRight>
-              </Nav>
-          </Navbar>
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#home">ShoppingMall</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem href="#menu1">Menu1</NavItem>
+                        <NavItem href="#menu2">Menu2</NavItem>
+                    </Nav>
+                    <Nav pullRight>
+                        <Button>LOGIN</Button>
+                        <Button>REGISTER</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
-    }
+    };
 }
 export default MainNav;
