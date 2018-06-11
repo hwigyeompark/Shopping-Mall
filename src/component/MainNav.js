a521708209f401b657747ab914f2336da13aa2fc
hwigyeompark <phg5590@gmail.com>
2018-06-11 11:15:04 +0900

    update main

--- src/component/MainNav.js
+++ src/component/MainNav.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Navbar } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class MainNav extends Component(){
+class MainNav extends Component{
     render(){
         return(
-          <Navbar/>
+          <Navbar className="nav navbar-default">
+              <div className="container-fluid">
+                  <a className="navbar-brand">ShoppingMall</a>
+                  <div className="collapse navbar-collapse">
+                      <ul className="nav navbar-nav">
+                          <li className="active">메뉴1</li>
+                          <li className="active">메뉴2</li>
+                          <button type="button" className="btn btn-default navbar-btn">로그인</button>
+                          <button type="button" className="btn btn-default navbar-btn">회원가입</button>
+                      </ul>
+                  </div>
+              </div>
+          </Navbar>
         );
     }
 }
