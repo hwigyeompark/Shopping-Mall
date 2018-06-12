4f0ac3ea1130f2ad213b36d2a986fb25b608906a
hwigyeompark <phg5590@gmail.com>
2018-06-13 05:56:20 +0900

    update mainNav

--- src/component/MainNav.js
+++ src/component/MainNav.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Nav, NavItem, NavLink, Navbar, NavbarBrand, Collapse, NavbarToggler } from 'reactstrap';
+import { Nav, NavItem, NavLink, Navbar, NavbarBrand, Collapse, NavbarToggler, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MainNav extends Component{
@@ -17,20 +17,31 @@ class MainNav extends Component{
         });
     }
     render(){
+        const style={
+            buttonGroup : {
+              marginRight:'10px'
+            },
+            button : {
+                marginLeft:'5px'
+            }
+        };
         return(
             <Navbar color="light" light expand="md">
                 <NavbarBrand href="./Main">ShoppingMall</NavbarBrand>
                 <NavbarToggler onClick={this.toggle}/>
                 <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" navbar>
+                    <Nav navbar>
                         <NavItem>
                             <NavLink href="/menu1">menu1</NavLink>
                         </NavItem>
                         <NavItem>
                             <NavLink href="/menu2">menu2</NavLink>
                         </NavItem>
-                        <NavItem>
-
+                    </Nav>
+                    <Nav className="ml-auto">
+                        <NavItem style={style.buttonGroup}>
+                            <Button style={style}>LOGIN</Button>
+                            <Button style={style.button}>REGISTER</Button>
                         </NavItem>
                     </Nav>
                 </Collapse>
