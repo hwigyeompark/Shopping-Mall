0524e37577f737678e03ce8684a0caa98e608218
hwigyeompark <phg5590@gmail.com>
2018-06-18 23:53:13 +0900

    update Slide

--- src/component/Slide.js
+++ src/component/Slide.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
 import back1 from '../img/back1.png';
 import back2 from '../img/back2.png';
-import back3 from '../img/back2.jpg';
+import back3 from '../img/back3.png';
 
 const items = [
     {
