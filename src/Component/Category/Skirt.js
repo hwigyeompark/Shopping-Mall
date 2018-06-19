cc2be611a9a770da5a3a31dcf20e73de54e24928
hwigyeompark <phg5590@gmail.com>
2018-06-19 14:53:04 +0900

    add SubNav

--- src/Component/Category/Skirt.js
+++ src/Component/Category/Skirt.js
@@ -1,30 +1,12 @@
 import React, {Component} from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import SubNav from "../SubNav";
+
 
 class Skirt extends Component{
     render(){
         return(
             <div>
-                <p>List Based</p>
-                <Nav vertical>
-                    <NavItem>
-                        <NavLink href="#">Link</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">Link</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#">Another Link</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink disabled href="#">Disabled Link</NavLink>
-                    </NavItem>
-                </Nav>
-                <hr />
-                <p>Link based</p>
-                <Nav vertical>
-                    <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink> <NavLink href="#">Another Link</NavLink> <NavLink disabled href="#">Disabled Link</NavLink>
-                </Nav>
+                <SubNav/>
             </div>
         );
     }
