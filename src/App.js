2c7fcc499d5bab7da474a88f5b5bcc8c3d1499a9
hwigyeompark <phg5590@gmail.com>
2018-06-19 14:20:52 +0900

    update MainNav, add Skirt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Main from "./component/Main";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Skirt from './component/Skirt';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <BrowserRouter>
+              <Route path='/skirt' component={Skirt}/>
+          </BrowserRouter>
           <Header/>
-            <Main/>
+          <Main/>
           <Footer/>
       </div>
     );
--- src/component/MainNav.js
+++ src/component/MainNav.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import '../css/MainNav.css';
 
-
 class MainNav extends Component{
     render(){
         return(
@@ -10,7 +9,7 @@ class MainNav extends Component{
                   <li><a href="#">아우터</a></li>
                   <li><a href="#">셔츠/블라우스</a></li>
                   <li><a href="#">팬츠</a></li>
-                  <li><a href="#">스커트</a></li>
+                  <li><a href='/skirt'>스커트</a></li>
                   <li><a href="#">악세사리</a></li>
                   <li><a href="#">신발</a></li>
               </ul>
