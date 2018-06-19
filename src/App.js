2cba99d8cb3a03eafaa7e27fca6900d089cb1e57
hwigyeompark <phg5590@gmail.com>
2018-06-19 10:14:41 +0900

    add Content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Main from "./component/Main";
-import MainNav from "./component/MainNav";
 import Header from "./component/Header";
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Header/>
-            <MainNav/>
             <Main/>
       </div>
     );
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Slide from "./Slide";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MainNav from "./MainNav";
+import Content from "./Content";
 
 class Main extends Component {
     render() {
         return (
             <div>
+                <MainNav/>
                 <Slide/>
+                <Content/>
             </div>
         );
     }
