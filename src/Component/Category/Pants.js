9f82b9b8402ade33f0f359ba19c459c094da1a48
hwigyeompark <phg5590@gmail.com>
2018-06-25 16:40:05 +0900

    update Pants and PantsDetail

--- src/Component/Category/Pants.js
+++ src/Component/Category/Pants.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Card, Button, CardImg, CardTitle, CardText, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
+import { Card, Button, CardImg, CardTitle, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
 import {withRouter} from 'react-router-dom';
 import '../../css/Pants.css';
 import SubNav from "../SubNav";
@@ -19,30 +19,30 @@ class Pants extends Component{
                 <SubNav/>
                 <div id="pants">
                     <CardDeck>
-                        <div className="pantsCard">
+                        <div className="pantsCard" onClick={this.detailView}>
                             <Card>
                                 <CardImg src={p1}/>
                                 <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
+                                    <CardTitle>유니크 일자 PT</CardTitle>
+                                    <CardSubtitle>18,000원</CardSubtitle>
                                 </CardBody>
                             </Card>
                         </div>
-                        <div className="pantsCard">
+                        <div className="pantsCard" onClick={this.detailView}>
                             <Card>
                                 <CardImg src={p2}/>
                                 <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
+                                    <CardTitle>심플 와이드 슬랙스</CardTitle>
+                                    <CardSubtitle>11,900원</CardSubtitle>
                                 </CardBody>
                             </Card>
                         </div>
-                        <div className="pantsCard">
+                        <div className="pantsCard" onClick={this.detailView}>
                             <Card>
                                 <CardImg src={p3}/>
                                 <CardBody>
-                                    <CardTitle>Card title</CardTitle>
-                                    <CardSubtitle>Card subtitle</CardSubtitle>
+                                    <CardTitle>크림치즈 일자 부츠컷</CardTitle>
+                                    <CardSubtitle>23,900원</CardSubtitle>
                                 </CardBody>
                             </Card>
                         </div>
--- src/Component/Detail/PantsDetail.js
+++ src/Component/Detail/PantsDetail.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
-import pants1 from '../../img/pants1.gif';
+import { Card, CardImg } from 'reactstrap';
 import '../../css/PantsDetail.css';
 import SubNav from "../SubNav";
+import p1 from '../../img/pants1.gif';
+import p2 from '../../img/pants2.gif';
+import p3 from '../../img/pants3.gif';
 
 class PantsDetail extends Component{
+    static defaultProps = {
+        img : 'No Img',
+        name : 'No Name',
+        company : 'No Company',
+        point : 'No Point',
+        price : 'No Price'
+    };
+
     constructor(props){
         super(props);
         this.state = {
@@ -43,26 +53,26 @@ class PantsDetail extends Component{
                 <SubNav/>
                 <div id="detailPants">
                     <Card id="pantsCard">
-                        <CardImg src={pants1}/>
+                        <CardImg src={this.props.img}/>
                     </Card>
 
                     <div id="pantsMent">
-                        <h5>유니크 일자PT</h5>
+                        <h5>{this.props.name}</h5>
                         <hr/>
                         <form>
                             <table>
                                 <tbody>
                                 <tr>
                                     <td>제조사 :</td>
-                                    <td>(주)바지청년들</td>
+                                    <td>{this.props.company}</td>
                                 </tr>
                                 <tr>
                                     <td>마일리지 :</td>
-                                    <td>180점 적립</td>
+                                    <td>{this.props.point}</td>
                                 </tr>
                                 <tr>
                                     <td>가격 :</td>
-                                    <td>18,000</td>
+                                    <td>{this.props.price}</td>
                                 </tr>
                                 <tr>
                                     <td>수량 :</td>
@@ -78,5 +88,40 @@ class PantsDetail extends Component{
             </div>
         );
     }
+}
+
+class PantsList extends Component{
+    render(){
+        const plist = [
+            {
+                img : p1,
+                name : '유니크 일자 PT',
+                company : '(주)바지청년들',
+                point : '180점',
+                price : '18,000'
+            },
+            {
+                img : p2,
+                name : '심플 와이드 슬랙스',
+                company : '(주)바지청년들',
+                point : '180점',
+                price : '18,000'
+            },
+            {
+                img : p3,
+                name : '크림치즈 일자 부즈컷',
+                company : '(주)바지청년들',
+                point : '180점',
+                price : '18,000'
+            }
+        ];
+        return(
+            <div>
+                {
+                    plist.map(pants => <PantsList goods={pants} img={pants.img} name={pants.name} company={pants.company} point={pants.point} price={pants.point}/>)
+                }
+            </div>
+        )
+    };
 }
 export default PantsDetail;
