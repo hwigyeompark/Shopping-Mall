591dc33f2e91a1f9d46d8ea00df08ad97a70395b
hwigyeompark <phg5590@gmail.com>
2018-07-02 23:40:56 +0900

    add server

---
+++ server/main.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+let port = 3000;
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.get('/hello', (req, res) => {
+    return res.send('Hello CodeLab');
+});
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port);
+});
